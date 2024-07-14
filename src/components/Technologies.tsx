df5651e0b488bb88ebb3911c20fb01ff4f72d21f
Saulo Cavalcante Coimbra <121812117+SauloCoimbr@users.noreply.github.com>
2024-07-14 20:29:38 -0300

    fix:project

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -25,7 +25,7 @@ const Technologies: React.FC = () => {
 
   return (
     <div className="container mx-auto p-4">
-      <h2 className="text-3xl font-bold mb-8">Tecnologias</h2>
+      <h2 className="text-3xl font-bold mb-8">Projetos</h2>
       <Carousel slides={slides} />
     </div>
   );
