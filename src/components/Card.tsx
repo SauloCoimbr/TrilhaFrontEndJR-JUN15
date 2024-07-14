4e42132877351ffb88cc067eed89ab6fbad2d84d
Saulo Cavalcante Coimbra <121812117+SauloCoimbr@users.noreply.github.com>
2024-07-14 20:02:16 -0300

    fix:border and text-color cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,10 +8,10 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ title, description, children }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white dark:bg-gray-800 m-4">
+    <div className="max-w-sm rounded-2xl overflow-hidden shadow-2xl bg-white dark:bg-gray-800 m-4">
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2 text-gray-900 dark:text-gray-200">{title}</div>
-        <p className="text-gray-700 dark:text-gray-400 text-base">
+        <div className="font-bold text-xl mb-2 text-gray-900 dark:text-gray-400">{title}</div>
+        <p className="text-gray-600 dark:text-gray-400 text-base">
           {description}
         </p>
       </div>
