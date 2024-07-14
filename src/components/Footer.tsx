1788508d3aaf585512339492dd95bc2cc57fd13b
Saulo Cavalcante Coimbra <121812117+SauloCoimbr@users.noreply.github.com>
2024-07-14 20:06:39 -0300

    fix:text-size footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="text-codigo-certo-red py-4">
+    <footer className="text-codigo-certo-red py-1">
       <div className="container mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} Criado por Saulo. Todos os direitos reservados.</p>
+        <p className='text-xs'>&copy; {new Date().getFullYear()} Criado por Saulo Coimbra. Todos os direitos reservados.</p>
       </div>
     </footer>
   );
