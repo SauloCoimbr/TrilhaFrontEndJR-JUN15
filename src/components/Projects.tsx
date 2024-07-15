b67acad51b741f07033f2f7ec7591e0c45ba9d07
Saulo Cavalcante Coimbra <121812117+SauloCoimbr@users.noreply.github.com>
2024-07-14 21:15:22 -0300

    fix:project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Card from './Card';
 import Badge from './Badge';
+import { FaReact } from 'react-icons/fa';
+import { RiTailwindCssFill } from "react-icons/ri";
+import { TbBrandTypescript } from "react-icons/tb";
+import { CiLink } from "react-icons/ci";
 
 const Projects: React.FC = () => {
   return (
@@ -8,19 +12,19 @@ const Projects: React.FC = () => {
       <h2 className="text-3xl font-bold mb-4 mt-4">Meus Projetos</h2>
       <div className="flex flex-wrap">
         <Card title="PanoramaCars" description="Este é um projeto incrível que utiliza React e Tailwind CSS, nele temos como proposta, compra e venda de veículos">
-          <Badge color="primary" to="https://project-x-tan-tau.vercel.app/login">React</Badge>
-          <Badge color="secondary" to="https://project-x-tan-tau.vercel.app/login">Tailwind CSS</Badge>
-          <Badge color="danger" to="https://project-x-tan-tau.vercel.app/login">Link</Badge>
+          <Badge className='gap-2' color="primary"><FaReact/>React.Js</Badge>
+          <Badge className='gap-2' color="secondary"><RiTailwindCssFill />Tailwind CSS</Badge>
+          <Badge className='gap-2' color="danger" to="https://project-x-tan-tau.vercel.app/login"><CiLink />Link</Badge>
         </Card>
         <Card title="PanoramaBurguer" description="Outro projeto incrível que utiliza ReactJs, Tailwind CSSs e TypeScript, nele temos como proposta, compra e venda de alimentos">
-          <Badge color="primary" to="https://entrega-s3-template-hamburgueria-saulo-coimbra.vercel.app/">React.js</Badge>
-          <Badge color="secondary" to="https://entrega-s3-template-hamburgueria-saulo-coimbra.vercel.app/">TypeScript</Badge>
-          <Badge color="danger" to="https://entrega-s3-template-hamburgueria-saulo-coimbra.vercel.app/">Link</Badge>
+          <Badge className='gap-2' color="primary"><FaReact/>React.js</Badge>
+          <Badge className='gap-2' color="secondary"><TbBrandTypescript />TypeScript</Badge>
+          <Badge className='gap-2' color="danger" to="https://entrega-s3-template-hamburgueria-saulo-coimbra.vercel.app/"><CiLink />Link</Badge>
         </Card>
         <Card title="PanoramaToDo" description="Outro projeto incrível que utiliza React.js, JavaScript e SaSS, nele temos como proposta uma lista de tarefas.">
-          <Badge color="primary" to="https://react-entrega-s1-template-nu-kenzie-saulo-coimbra.vercel.app/">React.js</Badge>
-          <Badge color="secondary" to="https://react-entrega-s1-template-nu-kenzie-saulo-coimbra.vercel.app/">TypeScript</Badge>
-          <Badge color="danger" to="https://react-entrega-s1-template-nu-kenzie-saulo-coimbra.vercel.app/">Link</Badge>
+          <Badge className='gap-2' color="primary"><FaReact/>React.js</Badge>
+          <Badge className='gap-2' color="secondary"><TbBrandTypescript />TypeScript</Badge>
+          <Badge className='gap-2' color="danger" to="https://react-entrega-s1-template-nu-kenzie-saulo-coimbra.vercel.app/"><CiLink />Link</Badge>
         </Card>
       </div>
     </div>
